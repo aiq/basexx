8c46f4e17dfdb5d0fd54858c5cd9cd417b96432a
Alexander Schulz <alex.schulz.mail@googlemail.com>
2013-10-20 12:18:05 +0200

    Implemented base2, base32 and base64 with tests

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-basexx
-======
+#basexx
 
-A library which provides base2, base32(crock ford/rfc), base64 decoding and encoding.
+A library which provides base2(bitfield), base32(crock ford/rfc 3548), base64 decoding and encoding.
+
+##from_bit / to_bit
+
+Converts a byte string to a bitfield string.
+
+* 0, O and o maps to the same value
+* 1, I, i, L and l maps to the same value
+
+<!-- mou problem -->
+
+	basexx.to_bit( "ACDC" ) --> 01000001010000110100010001000011
+	basexx.from_bit( "01000001010000110100010001000011" ) --> ACDC
+	basexx.from_bit( "o1ooooo1o1oooo11" ) --> AC
+	basexx.from_bit( "OioooooiOiooooii" ) --> AC
+
+##from_hex / to_hex
+
+Converts a byte string to a uppercase hex data string.
+
+	basexx.to_hex( "Hello world!" ) --> 48656C6C6F20776F726C6421
+	basexx.from_hex( "48656C6C6F20776F726C6421" ) --> Hello world!
+	basexx.from_hex( "48656c6c6f20776f726c6421" ) --> Hello world!
+
+##from_base32 / to_base32
+
+Converts a byte string to a base32(_rfc3548_) uppercase data string.
+
+* It's case insensitive
+
+<!-- mou problem -->
+
+	basexx.to_base32( "chunky bacon!" ) --> MNUHK3TLPEQGEYLDN5XCC===
+	basexx.from_base32( "MNUHK3TLPEQGEYLDN5XCC===" ) --> chunky bacon!
+
+##from_crockford / to_crockford
+
+Converts a byte string to a base32(_crockford_) uppercase data string. The optional check value is not implemented.
+
+* It's case insensitive
+* 1, I, i, L and l maps to the same value
+* 0, O and o maps to the same value
+* U, u, V and v maps to the same value (not part of the official description)
+
+<!-- mou problem -->
+
+	string.lower( basexx.to_crockford( "Hello World" ) ) --> 91jprv3f41bpywkccg
+	basexx.from_crockford( "axqqeb10d5u20wk5c5p6ry90exqq4uvk44" ) --> Wow, it really works!
+
+##from_base64 / to_base64
+
+Converts a byte string to a base64 data string.
+
+	basexx.to_base64( "Man") --> TWFu
+	basexx.from_base64( "TWFu" ) --> Man
---
+++ lib/basexx.lua
@@ -0,0 +1,169 @@
+------------------------------------------------------------------------------------------------------------------------
+-- util functions
+------------------------------------------------------------------------------------------------------------------------
+
+function divide_string( str, max, fillChar )
+   fillChar = fillChar or ""
+   local result = {}
+
+   local start = 1
+   for i = 1, #str do
+      if i % max == 0 then
+         table.insert( result, str:sub( start, i ) )
+         start = i + 1
+      elseif i == #str then
+         table.insert( result, str:sub( start, i ) )
+      end
+   end
+
+   return result
+end
+
+function number_to_bit( num, length )
+   local bits = {}
+
+   while num > 0 do
+      local rest = math.fmod( num, 2 )
+      table.insert( bits, rest )
+      num = ( num - rest ) / 2
+   end
+
+   while #bits < length do
+      table.insert( bits, "0" )
+   end
+
+   return string.reverse( table.concat( bits ) )
+end
+
+------------------------------------------------------------------------------------------------------------------------
+
+local basexx = {}
+
+------------------------------------------------------------------------------------------------------------------------
+-- base2(bitfield) decode and encode function
+------------------------------------------------------------------------------------------------------------------------
+
+function basexx.from_bit( str )
+   str = string.lower( str )
+   str = str:gsub( 'o', '0' )
+   str = str:gsub( 'l', '1' )
+   str = str:gsub( 'i', '1' )
+   return ( str:gsub( '........', function ( cc )
+               return string.char( tonumber( cc, 2 ) )
+            end ) )
+end
+
+function basexx.to_bit( str )
+   return ( str:gsub( '.', function ( c )
+               local byte = string.byte( c )
+               local bits = {}
+               for i = 1,8 do
+                  table.insert( bits, byte % 2 )
+                  byte = math.floor( byte / 2 )
+               end
+               return table.concat( bits ):reverse()
+            end ) )
+end
+
+------------------------------------------------------------------------------------------------------------------------
+-- base16(hex) decode and encode function
+------------------------------------------------------------------------------------------------------------------------
+
+function basexx.from_hex( str )
+   return ( str:gsub( '..', function ( cc )
+               return string.char( tonumber( cc, 16 ) )
+            end ) )
+end
+
+function basexx.to_hex( str )
+   return ( str:gsub( '.', function ( c )
+               return string.format('%02X', string.byte( c ) )
+            end ) )
+end
+
+------------------------------------------------------------------------------------------------------------------------
+-- generic function to decode and encode base32/base64
+------------------------------------------------------------------------------------------------------------------------
+
+local function from_basexx( str, alphabet, bits )
+   local result = {}
+   for i = 1, #str do
+      local c = string.sub( str, i, i )
+      if c ~= '=' then
+         local index = string.find( alphabet, c )
+         table.insert( result, number_to_bit( index - 1, bits ) )
+      end
+   end
+
+   local value = table.concat( result )
+   local pad = #value % 8
+   return basexx.from_bit( string.sub( value, 1, #value - pad ) )
+end
+
+local function to_basexx( str, alphabet, bits, pad )
+   local bitString = basexx.to_bit( str )
+
+   local chunks = divide_string( bitString, bits )
+   local result = {}
+   for key,value in ipairs( chunks ) do
+      if ( #value < bits ) then
+         value = value .. string.rep( '0', bits - #value )
+      end
+      local pos = tonumber( value, 2 ) + 1
+      table.insert( result, alphabet:sub( pos, pos ) )
+   end
+
+   table.insert( result, pad )
+   return table.concat( result )   
+end
+
+------------------------------------------------------------------------------------------------------------------------
+-- rfc 3548: http://www.rfc-editor.org/rfc/rfc3548.txt
+------------------------------------------------------------------------------------------------------------------------
+
+local base32Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+
+function basexx.from_base32( str )
+   return from_basexx( string.upper( str ), base32Alphabet, 5 )
+end
+
+function basexx.to_base32( str )
+   return to_basexx( str, base32Alphabet, 5, ({ '', '======', '====', '===', '=' })[ #str % 5 + 1 ] )
+end
+
+------------------------------------------------------------------------------------------------------------------------
+-- crockford: http://www.crockford.com/wrmg/base32.html
+------------------------------------------------------------------------------------------------------------------------
+
+local crockfordAlphabet = "0123456789ABCDEFGHJKMNPQRTUVWXYZ"
+
+function basexx.from_crockford( str )
+   str = string.upper( str )
+   str = string.gsub( str, "I", "1" )
+   str = string.gsub( str, "L", "1" )
+   str = string.gsub( str, "O", "0" )
+   str = string.gsub( str, "S", "5”" )
+   return from_basexx( str, crockfordAlphabet, 5 )
+end
+
+function basexx.to_crockford( str )
+   return to_basexx( str, crockfordAlphabet, 5, "" )
+end
+
+------------------------------------------------------------------------------------------------------------------------
+-- base64 decode and encode function
+------------------------------------------------------------------------------------------------------------------------
+
+local base64Alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+ 
+function basexx.from_base64( str )
+   return from_basexx( str, base64Alphabet, 6 )
+end
+
+function basexx.to_base64( str )
+   return to_basexx( str, base64Alphabet, 6, ({ '', '==', '=' })[ #str % 3 + 1 ] )
+end
+
+------------------------------------------------------------------------------------------------------------------------
+
+return basexx
