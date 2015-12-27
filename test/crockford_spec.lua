16328ee08a6c8dbc58da0131df82ca8caba2a8eb
Alexander Schulz <aiqpost@yahoo.com>
2015-12-27 22:51:34 +0100

    Fixed outdated README example for crockford and added tests for the crockford examples.

--- test/crockford_spec.lua
+++ test/crockford_spec.lua
@@ -8,4 +8,11 @@ describe( "should handle base32(crockford) strings", function()
       assert.is.same( "foo", basexx.from_crockford( "CSQPY" ) )
    end)
 
+   it( "should really work ;-)", function()
+      -- https://github.com/aiq/basexx/issues/3
+      assert.is.same( "91jprv3f41bpywkccg", string.lower( basexx.to_crockford( "Hello World" ) ) )
+      assert.is.same( "AXQQEB10D5T20WK5C5P6RY90EXQQ4TVK44", basexx.to_crockford( "Wow, it really works!" ) )
+      assert.is.same( "Wow, it really works!", basexx.from_crockford( "axqqeb10d5t20wk5c5p6ry90exqq4tvk44" ) )
+   end)
+
 end)
